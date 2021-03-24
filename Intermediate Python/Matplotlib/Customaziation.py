25f4a60ee9b5a493aec0a3f3bf2d9482693f1998
Mohamed Yassine Elkhadiri <mohamedyassine.elkhadiri@insat.u-carthage.tn>
2021-03-25 00:26:54 +0100

    changed customaziation.py

--- Intermediate Python/Matplotlib/Customaziation.py
+++ Intermediate Python/Matplotlib/Customaziation.py
@@ -21,5 +21,13 @@ plt.ylabel(ylab)
 # Add title
 plt.title(title)
 
+# Definition of tick_val and tick_lab
+tick_val = [1000, 10000, 100000]
+tick_lab = ['1k', '10k', '100k']
+
+# Adapt the ticks on the x-axis
+plt.xticks(tick_val,tick_lab)
+
+
 # After customizing, display the plot
 plt.show()
